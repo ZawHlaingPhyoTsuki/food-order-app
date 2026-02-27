f6c7908afb191078c36c279bf1a31201205419eb
Zaw Hlaing Phyo <tro2233zhp@gmail.com>
2026-02-27 17:52:09 +0700

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,23 @@
+import prisma from "@food-order-app/db";
+import { env } from "@food-order-app/env/server";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [],
+});
